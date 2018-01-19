a0ffc7e1470fb20e31ff9c0f5dfbaa9efa549ccf
pj93coder <pj93coder@gmail.com>
2018-01-19 15:26:25 +0100

    Updating ID

--- MVCAssignmentLayoutandViews/Models/People.cs
+++ MVCAssignmentLayoutandViews/Models/People.cs
@@ -7,7 +7,8 @@ namespace MVCAssignmentLayoutandViews.Models
 {
     public class PeopleM
     {
-
+        static int id = 0;
+        private int ID { get; set; }
         public static List<PeopleM> Peoples {get;set;}
         public string Name { get; set; }
 
@@ -16,9 +17,9 @@ namespace MVCAssignmentLayoutandViews.Models
             Peoples = new List<PeopleM>();
         }
 
-        private ID()
+        private PeopleM()
         {
-
+            ID = id++;
         }
 
         public PeopleM (string name)
